1655cbd69ba69c34944ce4638da177bee8f0e7a1
Pranav-techhub <pranav.24sk@gmail.com>
2025-08-18 18:05:30 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,11 @@
 from flask import Flask
-from flask_cors import CORS
 from backend.routes import routes_bp
-from backend.payments import payments_bp
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
-    app.register_blueprint(routes_bp, url_prefix="/api")
-    app.register_blueprint(payments_bp, url_prefix="/api")
+    app.register_blueprint(routes_bp)
     return app
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
